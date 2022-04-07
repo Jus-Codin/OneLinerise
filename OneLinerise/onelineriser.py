3477746e68b098dfc8e9926137cd76fca3e06c39
Jus-Codin <70018166+Jus-Codin@users.noreply.github.com>
2022-04-07 12:18:53 +0800

    Renamed functions to reduce confusion

--- OneLinerise/onelineriser.py
+++ OneLinerise/onelineriser.py
@@ -1,13 +1,50 @@
 import builtins
 
+from __future__ import annotations
+
 class OneLineError(Exception): ...
 
 class _MISSING: ...
 
+def chainablemethod(func):
+  """Convienience decoratror which makes a method return its class"""
+  def wrapper(self, *args, **kwargs):
+    func(self, *args, **kwargs)
+    return self
+  return wrapper
 
 
-class ProxyObject:
-  """Hah try saying that ten times real quick"""
+
+class AttributedCallable:
+  """
+  Due to the confusion of using a property decorator for the chaining of functions, 
+  it is better to make a new class to tell the difference
+
+  This class decorator functions similar to `@property`, except that it is view only and cannot be set
+  """
+
+  def __init__(self, func):
+    self.func = func
+
+  def __get__(self, instance, owner=None):
+    if instance is None:
+      return self
+    return self.func(instance)
+
+
+
+class _ProxyObject:
+  """
+  Hah try saying that ten times real quick
+  This is not meant to be created by the user, its only used internally
+
+  Parameters
+  ----------
+  owner: :class:`OneLinerise`
+  value: `Any`
+
+
+  """
   
   def __init__(self, owner, value):
     self.owner = owner
@@ -25,25 +62,18 @@ class ProxyObject:
     self.value = self.value(*args, **kwargs)
     return self
   
-  @property
+  @AttributedCallable
   def RET_OBJ(self):
     """Stop getting items from current object, return to monke"""
+
     self.owner.last = self.value
     return self.owner
 
 
 
-def chainablemethod(func):
-  """Convienience decoratror which makes a method return its class"""
-  def wrapper(self, *args, **kwargs):
-    func(self, *args, **kwargs)
-    return self
-  return wrapper
-
-
-
 class OneLinerise:
   """Where the magic happens"""
+
   last = _MISSING
 
   def __init__(self, globals=globals()):
@@ -66,25 +96,28 @@ class OneLinerise:
     self.last = self.last(*args, **kwargs)
     return self
 
-  @property
+  @AttributedCallable
   @chainablemethod
   def save_last(self):
     """Saves the value of the last thing returned in '_'"""
     self._globals["_"] = self.last
 
-  @property
+  @AttributedCallable
   @chainablemethod
   def print_last(self):
     """Prints the last object returned"""
     print(self.last)
   
-  @property
+  @AttributedCallable
   def returned(self):
-    """Creates a proxy object which allows you to interact with the last object. To return to the main class run the `RET_OBJ`.
-    Yes, it's a bad way to do it, but does it look like I care?"""
+    """
+    Creates a proxy object which allows you to interact with the last object. To return to the main class run the `RET_OBJ`.
+    Yes, it's a bad way to do it, but does it look like I care?
+    """
+
     if self.last is _MISSING:
       raise OneLineError("nothing to return!")
-    return ProxyObject(self, self.last)
+    return _ProxyObject(self, self.last)
 
   @chainablemethod
   def save_last_as(self, name):
